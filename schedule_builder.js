7eb25ebac2274c7e8f4df893c5912aca2370ac31
Javier Orduz <jaorduz@ieee.org>
2026-03-14 00:08:07 -0600

    Add active event selector

--- schedule_builder.js
+++ schedule_builder.js
@@ -62,29 +62,23 @@ let html = `
 
 sessions.forEach(s=>{
 
-const zoom =
-s.zoomLink
-? `<a class="zoom-link" href="${s.zoomLink}" target="_blank">Join Session</a>`
-: "";
+// const zoom =
+// s.zoomLink ? `<a class="zoom-link" href="${s.zoomLink}" target="_blank">Join Session</a>`: "";
+const zoom = `<a class="zoom-link" href="${s.zoomLink || "#"}" target="_blank">Join Session</a>`;
 
 html += `
 <tr>
-
 <td>${escapeHtml_(s.time)}</td>
-
-<td class="talk-title">
-${escapeHtml_(s.title)}
-</td>
-
+<td class="talk-title">${escapeHtml_(s.title)}</td>
 <td>
 ${escapeHtml_(s.speaker)}<br>
 <span style="font-size:12px;color:#666">
 ${escapeHtml_(s.institution)}
 </span>
 </td>
-
-<td>${zoom}</td>
-
+<td>
+${zoom}
+</td>
 </tr>
 
 <tr>
