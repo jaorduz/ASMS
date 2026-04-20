10f59b9267908e3a19f7aa7a8f6d6e76393ba607
Javier Orduz <jaorduz@ieee.org>
2026-04-20 11:30:07 -0600

    Add apply to event utto to conference website

--- letter_schema.js
+++ letter_schema.js
@@ -60,8 +60,9 @@ function initializeLetterSheet_(){
   sheet.getRange(1, 1, 1, headers.length).setValues([headers]);
   sheet.setFrozenRows(1);
 
-  SpreadsheetApp.getUi().alert(
-    "Letter Invitation System",
-    "The letters_data sheet has been created successfully."
-  );
-}
+
+SpreadsheetApp.getUi().alert(
+  "Letter Invitation System",
+  "The letters_data sheet has been created successfully.",
+  SpreadsheetApp.getUi().ButtonSet.OK
+);
